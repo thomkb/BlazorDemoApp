629a917b877ba71bc568f478771f511cb7d7809c
Thomas Britt <thomas.k.britt@gmail.com>
2025-02-24 17:45:10 -0800

    Moved weather display to component; added WeatherDisplayData class; performing ETL while fetching data; added more bootstrap

--- BlazorDemo/BlazorDemo.Client/Classes/WeatherDataReader.cs
+++ BlazorDemo/BlazorDemo.Client/Classes/WeatherDataReader.cs
@@ -1,7 +1,9 @@
 ﻿using Models.WeatherData;
 using System.Text.Json;
+using System.Net;
+using System.Web;
 using Serilog;
-using Microsoft.AspNetCore.Components;
+using Models.WeatherDisplayData;
 
 
 namespace BlazorDemo.Classes;
@@ -9,30 +11,60 @@ namespace BlazorDemo.Classes;
 public class WeatherDataReader
 {
 
-    public async Task<WeatherDataFromApi>? ReadFromApi(string ApiPath, Serilog.ILogger _logger)
+    public async Task<WeatherDisplayData>? ReadFromApi(string ApiPath, bool isMetric, Serilog.ILogger _logger)
     {
-        var response = "";
+        var response = string.Empty;
         var httpClient = new HttpClient();
-       
+
         try
         {
             response = await httpClient.GetStringAsync(ApiPath);
-           _logger.Information("WeatherDataReader.ReadFromApi: api call: {api}", ApiPath);
-           _logger.Information("=====================================================");
+            _logger.Information("WeatherDataReader.ReadFromApi: {api}", ApiPath);
         }
-        catch (Exception ex) 
+        catch (HttpRequestException hex)
         {
-            var msg = ex.Message;
-            _logger.Error(ex, msg);
+            var query = HttpUtility.ParseQueryString(ApiPath);
+            var zipCode = query.Get("q");
+            if (hex.StatusCode == HttpStatusCode.BadRequest)
+            {
+                _logger.Error("Could not find weather for zip code '{zip}'", zipCode);
+            }
+            else if (hex.StatusCode == HttpStatusCode.NotFound)
+            {
+                _logger.Error("404 Error; Could not find the API: '{url}'", ApiPath);
+            }
+            else
+            {
+                _logger.Error(hex, hex.Message);
+            }
             response = null;
+        }
+
+        catch (Exception ex)
+        {
+            _logger.Error(ex, ex.Message);
+            response = null;
+        }
+        finally
+        {
+            _logger.Information("==============================================================================================================");
         }
 
         if (response == null)
         {
             return null;
-        }
+        } 
 
-        return JsonSerializer.Deserialize<WeatherDataFromApi>(response, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        var jData = JsonSerializer.Deserialize<WeatherDataFromApi>(response, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        if (jData != null)
+        {
+            WeatherDisplayData weatherDisplayData = new WeatherDisplayData().GetWeatherDisplayData(jData, isMetric);
+            return weatherDisplayData;
+        }
+        else
+        {
+            return null;
+        }
     }
 }
 
---
+++ BlazorDemo/BlazorDemo.Client/Models/WeatherDisplayData.cs
@@ -0,0 +1,70 @@
+﻿using Models.WeatherData;
+
+namespace Models.WeatherDisplayData;
+
+public class WeatherDisplayData
+{
+    public string? WeatherDescription { get; set; }
+    public string Wind { get; set; } = string.Empty;
+    public string WindDirection { get; set; } = string.Empty;
+    public string? WindDegree { get; set; } = string.Empty;
+    public string WindSpeed { get; set; } = string.Empty;
+    public string WindGust { get; set; } = string.Empty;
+    public string? City { get; set; }
+    public string? State { get; set; }
+    public string? Country { get; set; }
+    public string? PostalCode { get; set; }
+    public string? IconUrl { get; set; }
+    public string? TimeUpdated { get; set; }
+    public string? Temperature { get; set; } = string.Empty;
+    public string? Degree { get; set; } = string.Empty;
+    public string? Pressure { get; set; } = string.Empty;
+    public string? Precipitation { get; set; } = string.Empty;
+    public string? Humidity { get; set; } = string.Empty;
+    public string? FeelsLike { get; set; } = string.Empty;
+    public string? WindChill { get; set; } = string.Empty;
+    public string? HeatIndex { get; set; } = string.Empty;
+    public string? Dewpoint { get; set; } = string.Empty;
+    public string? Visibility { get; set; } = string.Empty;
+    public string? UvIndex { get; set; } = string.Empty;
+    public string? CloudCover { get; set; } = string.Empty;
+
+    public WeatherDisplayData() { }
+
+    public WeatherDisplayData GetWeatherDisplayData(WeatherDataFromApi weatherData, bool isMetric)
+    {
+        WeatherDisplayData weatherDisplayData = new WeatherDisplayData();
+
+        if (weatherData != null)
+        {
+            weatherDisplayData.WeatherDescription = weatherData.Current?.Condition?.Text ?? string.Empty;
+            weatherDisplayData.City = weatherData.Location?.Name ?? string.Empty;
+            weatherDisplayData.State = weatherData.Location?.Region ?? string.Empty;
+            weatherDisplayData.Country = weatherData.Location?.Country ?? string.Empty;
+            weatherDisplayData.PostalCode = weatherData.PostalCode;
+            weatherDisplayData.IconUrl = weatherData.Current?.Condition?.Icon ?? string.Empty;
+            weatherDisplayData.TimeUpdated = weatherData.Current?.LastUpdated ?? string.Empty;
+            weatherDisplayData.Humidity = $"{weatherData.Current?.Humidity}%";
+            weatherDisplayData.Temperature = isMetric ? $"{weatherData.Current?.TempC} °C" : $"{weatherData.Current?.TempF} °F";
+            weatherDisplayData.Wind = isMetric ? $"{weatherData.Current?.WindKph} km/h" : $"{weatherData.Current?.WindMph} mph";
+            weatherDisplayData.WindDirection = weatherData.Current?.WindDir ?? string.Empty;
+            weatherDisplayData.WindSpeed = isMetric ? $"{weatherData.Current?.WindKph} km/h" : $"{weatherData.Current?.WindMph} mph";
+            weatherDisplayData.WindDegree = $"{weatherData.Current?.WindDegree}°";
+            weatherDisplayData.WindGust = isMetric ? $"{weatherData.Current?.GustKph} km/h" : $"{weatherData.Current?.GustMph} mph";
+            weatherDisplayData.Pressure = isMetric ? $"{weatherData.Current?.PressureMb} mb" : $"{weatherData.Current?.PressureIn} in";
+            weatherDisplayData.UvIndex = weatherData.Current?.Uv.ToString() ?? string.Empty;
+            weatherDisplayData.Visibility = isMetric ? $"{weatherData.Current?.VisKm} km" : $"{weatherData.Current?.VisMiles} miles";
+            weatherDisplayData.FeelsLike = isMetric ? $"{weatherData.Current?.FeelsLikeC} °C" : $"{weatherData.Current?.FeelsLikeF} °F";
+            weatherDisplayData.WindChill = isMetric ? $"{weatherData.Current?.WindchillC} °C" : $"{weatherData.Current?.WindchillF} °F";
+            weatherDisplayData.HeatIndex = isMetric ? $"{weatherData.Current?.HeatIndexC} °C" : $"{weatherData.Current?.HeatIndexF} °F";
+            weatherDisplayData.Dewpoint = isMetric ? $"{weatherData.Current?.DewpointC} °C" : $"{weatherData.Current?.DewpointF} °F";
+            weatherDisplayData.Precipitation = isMetric ? $"{weatherData.Current?.PrecipMm} mm" : $"{weatherData.Current?.PrecipIn} in";
+        }
+        else
+        {
+            return null;
+        }
+
+        return weatherDisplayData;
+    }
+}
